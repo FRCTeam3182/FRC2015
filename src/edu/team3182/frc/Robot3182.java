0a0f0f15e45e44f6f98751c68946970a35745c3e
Nodcah <Nodcah@giidmail.com>
2015-01-17 23:36:16 -0500

    Started copying/tidying code from last year

---
+++ src/edu/team3182/frc/Robot3182.java
@@ -0,0 +1,56 @@
+/** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * FRC Team 3182 Athena's Warriors
+ *
+ * Our official code of complete awesomeness to control the universe. 
+ * This year's game: Recycle Rush
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package edu.team3182.frc;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot3182 extends IterativeRobot {
+
+    //Declaring thread variables
+    private DriveTrain driveTrainVar;
+    
+    //Declaring joystick variable
+    private Joystick buttonsJoystick;
+
+    //Declaring dashboard variable
+    public SmartDashboard dash;
+
+    /**
+     * Called once when the robot is turned on
+     */
+    public void robotInit() {
+        //Initialize the threads
+        driveTrainVar = new DriveTrain();
+        new Thread(driveTrainVar, "DriveTrain").start();
+    }
+
+    /**
+     * Called once when autonomous is triggered
+     */
+    public void autonomousInit() {
+
+    }
+
+    /**
+     * Called every 10ms while in teleop
+     */
+    public void teleopPeriodic() {
+
+    }
+
+    /**
+     * Called every 10ms while in test mode
+     */
+    public void testPeriodic() {
+
+    }
+
+}
---
+++ src/edu/team3182/frc/Sensors.java
@@ -0,0 +1,61 @@
+package edu.team3182.frc;
+
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Sensors extends Object implements Runnable {
+
+    private AnalogChannel leftRangeFinder;
+    private AnalogChannel rightRangeFinder;
+    private Encoder rightDriveEncoder;
+    private Encoder leftDriveEncoder;
+    double leftVoltage;
+    double rightVoltage;
+    volatile int shootDistance;
+    
+
+    public Sensors() {
+        //initializing everything
+        rightDriveEncoder = new Encoder(4, 3);
+        leftDriveEncoder = new Encoder(2, 1);
+        leftRangeFinder = new AnalogChannel(1, 1);
+        rightRangeFinder = new AnalogChannel(1, 2);
+        rightDriveEncoder.setDistancePerPulse(.08168);
+    }
+
+    public void run() {
+        while (true) {
+            leftVoltage = leftRangeFinder.getVoltage();
+            rightVoltage = rightRangeFinder.getVoltage();
+            
+            SmartDashboard.putNumber("Average Voltage left", leftVoltage);
+            SmartDashboard.putNumber("Average Voltage right", rightVoltage);
+            SmartDashboard.putNumber("Speed Right", rightDriveEncoder.getRate());
+            SmartDashboard.putNumber("Speed Left", leftDriveEncoder.getRate());
+            Timer.delay(.1);
+             
+//            if (Math.abs(leftVoltage-rightVoltage) > .3) {
+//                leftVoltage = 0;
+//                rightVoltage = 0;
+//            }
+            
+            //If voltage is between x and y, we're in the right position
+            if (leftVoltage >= .598 && leftVoltage <= .897) {
+            shootDistance = 1; 
+            //If voltage is between x and y, we're too far
+            } else if (leftVoltage >= .898) {
+            shootDistance = 2; 
+            //If voltage is between x and y, we;re too close
+            } else if (leftVoltage >= .001 && leftVoltage <= .597) {
+            shootDistance = 0; 
+            }
+            SmartDashboard.putNumber("shootDistance Variable", shootDistance);
+        }
+    }
+    
+    public synchronized int shootingDistance() {
+        return shootDistance;
+    }
+}
