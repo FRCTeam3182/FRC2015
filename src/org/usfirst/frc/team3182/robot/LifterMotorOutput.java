19ddaa91bdbabc56fea3aadf750d12239dbb8551
nodcah <noahdc@sbcglobal.net>
2015-01-29 21:03:18 -0500

    Added new class for SpeedController for lifter motors

---
+++ src/org/usfirst/frc/team3182/robot/LifterMotorOutput.java
@@ -0,0 +1,34 @@
+package org.usfirst.frc.team3182.robot;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class LifterMotorOutput implements SpeedController{
+
+	private double speed;
+	
+	@Override
+	public void pidWrite(double output) {
+		speed = output;
+		
+	}
+
+	@Override
+	public double get() {
+		return speed;
+	}
+
+	@Override
+	public void set(double speed, byte syncGroup) {
+		this.speed = speed;
+	}
+
+	@Override
+	public void set(double speed) {
+		this.speed = speed;
+	}
+
+	@Override
+	public void disable() {
+		speed = 0;
+	}
+}
