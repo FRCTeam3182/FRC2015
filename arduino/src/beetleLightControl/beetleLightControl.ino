5937a6cd62da407cc0bb6bbccee862d8052f4cea
nodcah <nodcah@gmail.com>
2015-02-25 20:13:33 -0500

    Created new Arduino file

---
+++ arduino/src/beetleLightControl/beetleLightControl.ino
@@ -0,0 +1,19 @@
+/* BeetleLightControl by Noah Del Coro
+ *
+ * A program that recieves information from the RoboRIO and commands
+ * a strip of LEDs based off of set commands
+ *
+ */
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
+
+void delay(){
+  
+}
