65dfad8a56950c011e6b457ee3e1ac221e2afdfb
team3182 <team3182@gmail.com>
2015-02-28 11:40:52 -0500

    More changes

--- src/org/usfirst/frc/team3182/robot/ArduinoLights.java
+++ src/org/usfirst/frc/team3182/robot/ArduinoLights.java
@@ -11,66 +11,31 @@ package org.usfirst.frc.team3182.robot;
 
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
+import org.usfirst.frc.team3182.robot.lights.LightsEnum;
 
 
 public class ArduinoLights implements Runnable {
 
-	private boolean isAuto;
-    private int oldDistance;
-    private int distance;
 
-    //declare instances
-    private DriverStation driverStation;
-    //private Sensors sensors;
     private SerialPort arduino;
     private Port port;
 
     public ArduinoLights() {
         //initializing everything
         arduino = new SerialPort(9600, port); //I2C line for transmitting data
-        driverStation = DriverStation.getInstance();
     }
 
     public void run() {
         while (true) {
-            //store all of the data to variables
-            isAuto = driverStation.isAutonomous();
-//            distance = sensors.shootingDistance();
 
-            //how to send stuff
-             
-            
-            //store old variables to be used later
-            oldDistance = distance;
-            
-            //10ms loop delay
-            Timer.delay(.01);
         }
     }
 
-    public void setLightSequence(LightsEnum selection){
-        if (selection == LightsEnum.RANDOM){
-            randomSequence();
-        }
-        else if (selection == LightsEnum.PATTERN){
-            patternSequence();
-        }
-        else if (selection == LightsEnum.WITH_LIFTER){
-            withLifterSequence();
-        }
+    public synchronized void setLightSequence(LightsEnum selection){
+       sendID(selection.getID());
     }
 
-    private void randomSequence(){
-        // TODO Add light code for random sequence
-    }
-
-    private void patternSequence(){
-        // TODO Add pattern sequence
-    }
-
-    private void withLifterSequence(){
-        //TODO Add lifter sequence
-    }
+   private void sendID(int id){
+       arduino.writeString(String.valueOf(id));
+   }
 }
--- src/org/usfirst/frc/team3182/robot/LightsEnum.java
+++
@@ -1,6 +0,0 @@
-package org.usfirst.frc.team3182.robot;
-
-public enum LightsEnum {
-
-    WITH_LIFTER, RANDOM, PATTERN
-}
