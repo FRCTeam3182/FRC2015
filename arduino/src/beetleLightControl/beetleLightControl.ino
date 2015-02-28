a80984a5466e5fd22021bd82f564f71b06d2a2f4
Eric Zeiberg <ejzeiberg@gmail.com>
2015-02-28 16:27:07 -0500

    Start work on arduino side of lights code

--- arduino/src/beetleLightControl/beetleLightControl.ino
+++ arduino/src/beetleLightControl/beetleLightControl.ino
@@ -1,25 +1,42 @@
-/* BeetleLightControl by Noah Del Coro
+/* BeetleLightControl by Team 3182
  *
  * A program that recieves information from the RoboRIO and commands
  * a strip of LEDs based off of set commands
  *
  * Commands:
- * 
  *
+ * FYI This will be awesome
  *
  *
  *
  */
+ 
+struct RGB{
+  byte r;
+  byte g;
+  byte b;
+};
+
+int currentID = -1;
+boolean isTopLit = false;
+RGB topColor = {0,0,0};
 
 void setup() {
   Serial.begin(9600);
-  Serial.
 }
 
 void loop() {
-  
+
 }
 
-void delay(){
-  
+void wait(int wait) {
+  int tempMillis = millis();
+  while (millis() - tempMillis < wait) {
+    if (Serial.available() > 0) { //If something is received
+      if(Serial.
+    }
+    else {
+      delay(10);
+    }
+  }
 }
